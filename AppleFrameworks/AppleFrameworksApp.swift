aa10e73dc93968236cdee2e83c27fd402c6f38d9
ginaiscoding <rpaek88@gmail.com>
2022-12-19 11:42:22 -0800

    Initial Commit

---
+++ AppleFrameworks/AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksApp.swift
+//  AppleFrameworks
+//
+//  Created by Regina Paek on 12/19/22.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
