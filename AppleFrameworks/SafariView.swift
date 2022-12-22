fd8d2ce15cb61b3aa418b941b555e15d508cf01c
ginaiscoding <rpaek88@gmail.com>
2022-12-21 19:57:45 -0800

    added safari sheet

---
+++ AppleFrameworks/SafariView.swift
@@ -0,0 +1,23 @@
+//
+//  SafariView.swift
+//  AppleFrameworks
+//
+//  Created by Regina Paek on 12/21/22.
+//
+
+import Foundation
+import SwiftUI
+import SafariServices
+
+struct SafariView: UIViewControllerRepresentable {
+    
+    let url: URL
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<SafariView>) -> SFSafariViewController{
+        SFSafariViewController(url: url)
+    }
+    
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: UIViewControllerRepresentableContext<SafariView>) {
+        
+    }
+}
